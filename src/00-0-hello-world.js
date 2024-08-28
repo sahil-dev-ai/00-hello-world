cd46767a478549819786218307f1609dfc1b33d0
sahil-dev-ai <sahilnbadgewar@gmail.com>
2024-08-28 22:48:29 +0530

    updated initial commit
    error resolved

--- src/00-0-hello-world.js
+++ src/00-0-hello-world.js
@@ -10,5 +10,5 @@ function logWhatsUp() {
   // input: none
   // output: none
   // side effect: logs the string "What's up?" to the console
-  console.log("What's up?"); npm
+  console.log("What's up?");
 }
